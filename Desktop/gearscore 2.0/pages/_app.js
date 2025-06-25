aa848eefd7a50062c9eb6ed7bdd2284796a65c18
gamal123456789-dev <gamalkhaled9123@gmail.com>
2025-06-25 15:12:53 +0300

    Fix merge conflict and wrap with Layout + UserProvider

--- Desktop/gearscore 2.0/pages/_app.js
+++ Desktop/gearscore 2.0/pages/_app.js
@@ -1,22 +1,13 @@
 import '../styles/globals.css';
-<<<<<<< HEAD
 import Layout from '../components/Layout';
-import { UserProvider } from '../context/UserContext'; // ✅ أضف هذا
+import { UserProvider } from '../context/UserContext';
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <UserProvider> {/* ✅ لف كل حاجة داخل UserProvider */}
+    <UserProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </UserProvider>
   );
 }
-
-export default MyApp;
-=======
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
->>>>>>> 11c9ccf41cd8c56956f4218ba23161a8486c9d95
